625a4acca5b4f93bbbffb9a0e9df3f47ea2bb8df
胜者为王 <9370736+xiaguqian@user.noreply.gitee.com>
2026-05-13 19:59:29 +0800

    xunFood第二次提交

--- xunFood/backend/src/main/java/com/xunfood/entity/UserFollow.java
+++ xunFood/backend/src/main/java/com/xunfood/entity/UserFollow.java
@@ -1,3 +1,18 @@
 package com.xunfood.entity;
 
-import com.
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@TableName("user_follow")
+public class UserFollow {
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    private Long followerId;
+    private Long followingId;
+    private LocalDateTime createTime;
+}
